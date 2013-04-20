b08fdbe036f7cdea01bd82ec06560eb1e39db0b7
QilongTang <173288704@qq.com>
2013-04-20 16:18:50 -0400

    Upload Image Demo and DB

--- WebSite2/RestaurantInfo.aspx
+++ WebSite2/RestaurantInfo.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="RestaurantInfo.aspx.vb" Inherits="WebSite2_RestaurantInfo" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RestaurantInfo.aspx.cs" Inherits="WebSite2_RestaurantInfo" %>
 
 <!DOCTYPE html>
 
@@ -10,12 +10,21 @@
     <form id="form1" runat="server">
     <div>
     
-    </div>
-        <asp:ListView ID="ListView1" runat="server">
+        <asp:ListView ID="Pic_listView" runat="server">
         </asp:ListView>
-        <asp:Panel ID="Panel1" runat="server" Height="362px" Width="727px">
-            <asp:Image ID="Image1" runat="server" Height="349px" Width="710px" />
+        <asp:FileUpload ID="openFileDialog" runat="server" />
+        <br />
+        <asp:Label ID="UploadStatusLabel" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Button ID="butAdd" runat="server" Text="Insert" OnClick="butAdd_Click" />
+        <asp:Button ID="butDelete" runat="server" Text="Delete" />
+        <asp:Button ID="butClear" runat="server" Text="Clear" />
+        <asp:Button ID="butClose" runat="server" Text="Close" /><br /><br />
+        <asp:Panel ID="Panel1" runat="server" Height="287px" Width="565px">
+            <asp:Image ID="Image1" runat="server" Height="233px" Width="558px" />
         </asp:Panel>
+    
+    </div>
     </form>
 </body>
 </html>
---
+++ WebSite2/RestaurantInfo.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UIinterface.App_Code;
+
+public partial class WebSite2_RestaurantInfo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    public List<Image> pic = new List<Image>();//存放大图 
+    MyModule MyMC = new MyModule();
+
+
+    protected void butAdd_Click(object sender, EventArgs e)
+    {
+        MyMC.FileUpload(openFileDialog, UploadStatusLabel,Pic_listView);
+        /*
+        // Specify the path on the server to
+        // save the uploaded file to.
+        string savePath = @"c:\temp\uploads\";
+        //openFileDialog.AppRelativeTemplateSourceDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        //openFileDialog.filter = "bmp文件(*.bmp)|*.bmp";
+        if (openFileDialog.HasFile)
+        {
+
+                // Get the name of the file to upload.
+                string filename = openFileDialog.FileName;//获取图片路径
+                // Get the extension of the uploaded file.
+                string extension = System.IO.Path.GetExtension(filename);
+                // Allow only files with .doc or .xls extensions
+                // to be uploaded.
+                // Get the size in bytes of the file to upload.
+                int fileSize = openFileDialog.PostedFile.ContentLength;
+
+                if ((extension == ".bmp") || (extension == ".jpg"))
+                {
+                    if (fileSize < 2100000)
+                    {
+                        // Append the name of the file to upload to the path.
+                        savePath += filename;
+                        try
+                        {
+                            // Call the SaveAs method to save the 
+                            // uploaded file to the specified path.
+                            // This example does not perform all
+                            // the necessary error checking.               
+                            // If a file with the same name
+                            // already exists in the specified path,  
+                            // the uploaded file overwrites it.
+                            openFileDialog.SaveAs(savePath);
+
+                        }
+                        catch (System.Exception ex)
+                        {
+                            Response.Write("<script>alert(ex.Message)</script>");
+                            //MessageBox.Show(ex.Message);
+                            //MessageBox.Show("请选择图片文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                    else
+                    {
+                        UploadStatusLabel.Text = "Your file was not uploaded because " +
+                                         "it exceeds the 2 MB size limit.";
+                    }
+                }
+                else
+                {
+                    UploadStatusLabel.Text = "Your file was not Uploaded because it does not have a right extension";
+                }
+        }
+        else
+        {
+            UploadStatusLabel.Text = "You did not specify a file to upload.";
+        }*/
+    }
+}
